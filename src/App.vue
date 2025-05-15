12a355188fe35ae85bc58819730ae99c1c94bfa3
lizhongxian <1270863651@qq.com>
2025-05-15 20:45:43 +0800

    feat: 功能优化

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,31 @@
 <script setup>
+import { createApp } from 'vue'
 import HelloWorld from './components/HelloWorld.vue'
+console.log(HelloWorld)
+const helloWorldFnc = () => {
+  // console.log('helloWorldFnc')
+  // const el = document.createElement('div')
+  // const instance = createApp(HelloWorld, { msg: 'Vite + Vue',  modelValue: true,
+  //   'onUpdate:modelValue': (val) => {
+  //     if (!val) el.remove()
+  //   } })
+  // // instance.vm = instance.$mount()
+  // //  this.$el.appendChild(instance.vm.$el)
+  // // app.dialogVisible = true
+  // // console.log(app)
+  // // debugger
+  // instance.mount(el)
+  // document.body.appendChild(el)
+  // HelloWorld.setup()
+  // HelloWorld.render()
+
+}
+// const 
 </script>
 
 <template>
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+  <!-- <HelloWorld msg="3333" /> -->
+  <el-button type="primary" @click="helloWorldFnc()">创建一个组件</el-button>
 </template>
 
 <style scoped>
